9d38f61af4319b40327786146e4086ac3ade5264
HP <sreekanta.csd@gmail.com>
2022-07-10 09:16:52 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">save most of your money</h1>
+            <p className ="text-lg">By giving your money</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from '../Link/Link';
+
+const Navbar = () => {
+    const routes=[
+        {id: 1, name:"Home", link:"/home"},
+        {id: 1, name:"Service", link:"/service"},
+        {id: 1, name:"Deals", link:"/deals"},
+        {id: 1, name:"Coupons", link:"/coupons"},
+        {id: 1, name:"Contact", link:"/contact"},
+    ]
+    return (
+        <nav>
+            <ul>
+               {
+                routes.map(route => <Link
+                key={route.id}
+                route={route}
+                ></Link>)
+               }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
